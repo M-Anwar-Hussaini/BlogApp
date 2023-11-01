0d24eda6b04c244ef04163955c031672cec759e1
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-11-02 01:45:11 +0430

    Remove extra commits from routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,4 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
-
-  # get '/users', to: 'users#index'
-  # get '/users/:user_id', to: 'users#show', as: 'user'
-  # get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  # get '/users/:user_id/posts/:post_id', to: 'posts#show', as: 'user_post'
 end
