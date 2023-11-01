182bc701b74657ab9475b00ce638eacbc863008d
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-11-01 11:49:54 +0430

    Add tests for the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,29 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'associations' do
-    it { should have_many(:posts).with_foreign_key('author_id') }
-    it { should have_many(:comments) }
-    it { should have_many(:likes) }
+  before { subject.save }
+  it 'Name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
   end
 
-  describe 'validations' do
-    it { should validate_presence_of(:name) }
-    it { should validate_numericality_of(:posts_counter).only_integer.is_greater_than_or_equal_to(0) }
+  it 'is valid with a name' do
+    user = User.new(name: 'Hamid', posts_counter: 12)
+    expect(user).to be_valid
   end
 
-  describe 'recent_posts' do
-    it 'returns the most recent 3 posts' do
-      user = User.create(name: 'Test User')
-      # Create some test posts for the user with different creation times
-      user.posts.create(title: 'Post 1', created_at: 3.days.ago)
-      post2 = user.posts.create(title: 'Post 2', created_at: 2.days.ago)
-      post3 = user.posts.create(title: 'Post 3', created_at: 1.day.ago)
-      post4 = user.posts.create(title: 'Post 4', created_at: Time.now)
-
-      recent_posts = user.recent_posts
-
-      expect(recent_posts).to eq([post4, post3, post2])
-    end
+  it 'The counter should not be negative' do
+    user = User.new
+    user.name = 'Hamid'
+    user.posts_counter = -1
+    expect(user).to_not(be_valid)
   end
 end
