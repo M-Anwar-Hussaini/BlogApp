ad08562732bca844e3ae74a6462d62880c2cfa98
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-11-02 01:18:08 +0430

    Modify the routes to have a general structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/users', to: 'users#index'
-  get '/users/:user_id', to: 'users#show', as: 'user'
-  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  get '/users/:user_id/posts/:post_id', to: 'posts#show', as: 'user_post'
+
+  root "users#index"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+
+  # get '/users', to: 'users#index'
+  # get '/users/:user_id', to: 'users#show', as: 'user'
+  # get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  # get '/users/:user_id/posts/:post_id', to: 'posts#show', as: 'user_post'
 end
