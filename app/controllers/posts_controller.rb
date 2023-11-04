729466bd570dac2e435d715265c3c96208e52ad5
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-11-04 22:26:10 +0430

    Apply functionality for the post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,31 @@
 class PostsController < ApplicationController
+  before_action :set_user
+
+  def index
+    @posts = @user.posts
+  end
+
+  def new
+    @post = @user.posts.build
+  end
+
+  def create
+    @post = @user.posts.build(post_params)
+    if @post.save
+      redirect_to user_path(@user), notice: 'post was successfully added'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 
 end
