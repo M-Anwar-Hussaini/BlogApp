842dc514869ca45e104399201dbded85b506c4fe
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-11-14 23:59:40 +0430

    Add tests for post#index

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Posts index page', type: :feature do
   before(:each) do
-    @user = User.create(name: 'Anwar Hussaini', photo: 'https://github.com/account', bio: 'SoftWare developer', posts_counter: 0)
+    @user = User.create(name: 'User 1', photo: 'https://picsum.photos/300/300', bio: 'User 1 bio', posts_counter: 0)
     @post1 = Post.create(title: 'Post 1', text: 'Post 1 text', author: @user, comments_counter: 0, likes_counter: 0)
     @post2 = Post.create(title: 'Post 2', text: 'Post 2 text', author: @user, comments_counter: 0, likes_counter: 0)
     @post3 = Post.create(title: 'Post 3', text: 'Post 3 text', author: @user, comments_counter: 0, likes_counter: 0)
@@ -22,40 +22,27 @@ RSpec.describe 'Posts index page', type: :feature do
     profile = all('img')
     expect(profile.size).to eq(1)
   end
-  #
-  # scenario 'I can see the users name.' do
-  #   expect(page).to have_content(@user.name)
-  # end
-  #
-  # scenario 'I can see the number of posts the user has written.' do
-  #   expect(page).to have_content('Number of posts: 3')
-  # end
-  #
-  # scenario 'I can see a posts title.' do
-  #   expect(page).to have_content(@post1.title)
-  # end
-  #
-  # scenario 'I can see some of the posts body.' do
-  #   expect(page).to have_content(@post1.text)
-  # end
-  #
-  # scenario 'I can see the first comments on a post.' do
-  #   expect(page).to have_content('Comment 1')
-  #   expect(page).to have_content('Comment 2')
-  #   expect(page).to have_content('Comment 3')
-  # end
-  #
-  # scenario 'I can see how many comments a post has.' do
-  #   expect(page).to have_content('Comments: 3')
-  # end
-  #
-  # scenario 'I can see how many likes a post has.' do
-  #   expect(page).to have_content('Likes: 3')
-  # end
-  #
-  # scenario 'When I click on a post, it redirects me to that posts show page.' do
-  #   click_link @post1.title do
-  #     expect(page).to have_current_path(user_post_path(@user, @post1))
-  #   end
-  # end
+
+  scenario 'I can see the users name.' do
+    expect(page).to have_content(@user.name)
+  end
+
+  scenario 'I can see the number of posts the user has written.' do
+    expect(page).to have_content('Number of posts: 3')
+  end
+
+  scenario 'I can see a posts title.' do
+    expect(page).to have_content(@post1.title)
+  end
+
+  scenario 'I can see some of the posts body.' do
+    expect(page).to have_content(@post1.text)
+  end
+
+  scenario 'I can see the first comments on a post.' do
+    expect(page).to have_content('Comment 1')
+    expect(page).to have_content('Comment 2')
+    expect(page).to have_content('Comment 3')
+  end
+
 end
